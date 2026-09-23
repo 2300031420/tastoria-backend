a3ca9011f7406c8324a24141e093d4e5487734c9
Reddy Saneesh Sumesh <Sanukyj@gmail.com>
2026-09-23 13:29:50 +0530

    Update CORS frontend URLs in server.js
    Added new frontend URL for CORS configuration.

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const server = http.createServer(app);
 
 
 // ✅ Frontend URL for CORS
-const frontendURL = ["http://localhost:3000", "https://www.tastoria.in"];
+const frontendURL = ["http://localhost:3000", "https://www.tastoria.in",  "https://tastoria-front-liard.vercel.app"];
 
 
 const io = new Server(server, {
